d30b0cf3f21fa6a74b94827780543c141b911b00
venkatasai <venkatasaikadamati@gmail.com>
2025-11-03 19:13:51 +0530

    added boiler reader

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Counter is running")
+	// to read files and access system content, we use "os" package
+	// to read files we will use ReadFile from OS package
+	// syntax: func ReadFile(name string) ([]byte, error)
+	// since its exposed the func name would be starting with caps
+
+	data, err := os.ReadFile("documents/words.txt")
+	// data is slice of bytes, we need to type cast into ASCII string
+	fmt.Println("data", string(data)) // in a slice of bytes
+	fmt.Println(err)                  // if its nil, no errors, read is done
 }
